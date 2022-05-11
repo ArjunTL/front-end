929fc0cc5db9ff03c012fdd757a3e4bc967d477f
Milu <mks.c@saip.gov.sa>
2022-05-11 13:51:47 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp(
+    {
+       // template :'<h2> i am the template</h2>'
+       data(){
+           return{
+               title:' The Final Empire',
+               author:'brandon',
+               age:45
+           }
+       },
+       methods:
+       {
+        changeTitle(title)
+           {
+               this.title=title
+           }
+       }
+    }
+)
+
+app.mount('#app')
